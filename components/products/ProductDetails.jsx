a4a7e1c665c065f1adff86856bce89030bb5038f
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2024-01-06 21:41:24 +0530

    add to cart

--- components/products/ProductDetails.jsx
+++ components/products/ProductDetails.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./ProductDetails.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { addToCartHelper } from "@/lib/cart";
 
 const ProductDetails = ({ product }) => {
-  const { id, title, price, description, category, image, rating } = {
+  const router = useRouter();
+  const { data, status } = useSession();
+
+  const { id, title, price, description, image } = {
     ...product,
+  };
+
+  const addToCartHandler = async () => {
+    if (status === "unauthenticated") {
+      router.replace("/auth");
+      return;
+    }
+
+    const product = { id, title, price, image, quantity: 1 };
+
+    const response = await addToCartHelper({
+      product,
+      username: data.user.name,
+    });
+
+    if (response) {
+      router.replace("/cart");
+    }
   };
 
   return (
@@ -17,7 +41,7 @@ const ProductDetails = ({ product }) => {
         <div className={styles.productDetail}>
           <h3>$ {price}</h3>
           <p>{description}</p>
-          <button>Add to cart</button>
+          <button onClick={addToCartHandler}>Add to cart</button>
         </div>
       </div>
     </main>
