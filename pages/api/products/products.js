6b98aec034d8a0dfee6a4de5e803fe0bece25de5
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2024-01-06 17:52:49 +0530

    get product by id

--- pages/api/products/products.js
+++ pages/api/products/products.js
@@ -2,3 +2,8 @@ export const getAllProducts = async () => {
   const response = await fetch("https://fakestoreapi.com/products");
   return await response.json();
 };
+
+export const getProductById = async (id) => {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  return await response.json();
+};
