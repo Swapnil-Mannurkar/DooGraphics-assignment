b9e8110037e7122484b045fa48515794d5ee6182
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2024-01-08 02:24:38 +0530

    authentication

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,8 +1,25 @@
 import Checkout from "@/components/checkout/Checkout";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../api/auth/[...nextauth]";
 
 const CheckoutPage = () => {
   return <Checkout />;
 };
 
 export default CheckoutPage;
+
+export const getServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
